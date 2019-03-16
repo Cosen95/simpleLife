4808d5200774b99efc303638ac75dbc0d51a7828
fengshuan <1263215592@qq.com>
2019-03-16 17:19:06 +0800

    fix:http类封装通用异常处理

--- util/http.js
+++ util/http.js
@@ -1,5 +1,9 @@
 import { config } from '../config.js';
 
+const errCodeObj = {
+    1: '抱歉，出现了一个错误！'
+}
+
 class HTTP {
     request(params) {
         // url data method
@@ -16,13 +20,29 @@ class HTTP {
                 if(code.startsWith('2')) {
                     success(res.data)
                 } else {
-
+                    // 需根据服务器返回errcode显示对应报错信息，这里errcode默认给1
+                    this._show_error(1)
                 }
             },
             fail: (err) => {
-
+                this._show_error(1)
             }
         })
+    }
+
+    _show_error(error_code) {
+        wx.showToast({
+            title: errCodeObj[error_code],
+            icon: 'none',
+            image: '',
+            duration: 1500,
+            mask: false,
+            success: (result)=>{
+                
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+        });
     }
 }
 
