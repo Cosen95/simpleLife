15fc2c805794ab8db1a550e6254222e2b66529c1
fengshuan <1263215592@qq.com>
2019-03-16 13:00:24 +0800

    feat:组件properties属性

--- components/like/index.js
+++ components/like/index.js
@@ -1,17 +1,24 @@
 // components/like/index.js
+// 组件封闭性、开放性及粒度
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    check: {
+      type: Boolean
+    },
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    checkImg: 'images/like.png',
+    noCheckImg: 'images/like@dis.png'
   },
 
   /**
